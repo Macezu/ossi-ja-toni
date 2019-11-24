87e316629c4eda629fd49a55549d62b2de59605f
Marco Peciola <54833970+Macezu@users.noreply.github.com>
2019-11-24 10:54:53 +0200

    lohkottu selkeyden vuoksi

--- yritys.js
+++ yritys.js
@@ -1,1 +1,9 @@
-jsdhaj
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function kannykkanavi() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  } 
